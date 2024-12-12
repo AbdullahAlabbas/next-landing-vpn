709a820b45539d93371ad00a5c20d97f0b90ac91
Abdullah Alabbas <SEabdullahT@gmail.com>
2024-12-12 19:38:02 +0300

    .

--- components/Hero.js
+++ components/Hero.js
@@ -15,11 +15,23 @@ const Hero = () => {
           variants={scrollAnimation}
         >
           <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal" dir="rtl">
+            <h1
+              className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal"
+              dir="rtl"
+            >
               <strong>صناعة ثون INDUSTRIALTHON</strong>
             </h1>
             <p className="text-black-500 mt-4 mb-6" dir="rtl">
-              مسابقة تهدف إلى تعزيز التواصل بين المبدعين من جميع أنحاء المملكة وتطوير قدراتهم التقنية لتلبية احتياجات الصناعة برعاية فخرية من شركة أرامكو السعودية. تمثل "صناعة ثون" فرصة فريدة للمشاركين لتطوير مهاراتهم، تبادل الأفكار والمعرفة، وعرض مشاريعهم أمام خبراء الصناعة، حيث أن المشاريع الفائزة التي تُظهِر تأثيرًا إيجابيًا على عمليات أرامكو قد تحظى باهتمام شركة أرامكو السعودية لتبنيها، مما يوفر فرصة تطبيقها العملي في العالم الحقيقي. في "صناعة ثون" نهدف إلى بناء مجتمع متماسك من المبدعين السعوديين ودعم التكامل التقني بين المناطق، مما يسهم في رفع سقفِ معايير الابتكارات الوطنية في بيئة مشجعة.
+              مسابقة تهدف إلى تعزيز التواصل بين المبدعين من جميع أنحاء
+              المملكة وتطوير قدراتهم التقنية لتلبية احتياجات الصناعة برعاية
+              فخرية من شركة أرامكو السعودية. تمثل "صناعة ثون" فرصة فريدة
+              للمشاركين لتطوير مهاراتهم، تبادل الأفكار والمعرفة، وعرض مشاريعهم
+              أمام خبراء الصناعة، حيث أن المشاريع الفائزة التي تُظهِر تأثيرًا
+              إيجابيًا على عمليات أرامكو قد تحظى باهتمام شركة أرامكو السعودية
+              لتبنيها، مما يوفر فرصة تطبيقها العملي في العالم الحقيقي. في
+              "صناعة ثون" نهدف إلى بناء مجتمع متماسك من المبدعين السعوديين ودعم
+              التكامل التقني بين المناطق، مما يسهم في رفع سقفِ معايير
+              الابتكارات الوطنية في بيئة مشجعة.
             </p>
           </div>
           <div className="flex w-full">
@@ -38,76 +50,82 @@ const Hero = () => {
       </ScrollAnimationWrapper>
 
       {/* Logos Section */}
-      <div className="flex justify-end items-start mt-10 gap-10 md:gap-20">
-        {/* Sponsor */}
-        <div className="text-right">
-          <div className="mb-4 relative inline-block">
-            <h3
-              className="text-2xl font-bold relative z-10"
-            >
-              الراعي الرسمي
-            </h3>
-            <div
-              className="absolute w-full bg-orange-500 z-0"
-              style={{
-                height: '9px',
-                bottom: '0',
-                left: '0',
-              }}
-            ></div>
+      <ScrollAnimationWrapper>
+        <motion.div
+          className="flex justify-end items-start mt-10 gap-10 md:gap-20"
+          variants={scrollAnimation}
+        >
+          {/* Sponsor */}
+          <div className="text-right">
+            <div className="mb-4 relative inline-block">
+              <h3 className="text-2xl font-bold relative z-10">
+                الراعي الرسمي
+              </h3>
+              <div
+                className="absolute w-full bg-orange-500 z-0"
+                style={{
+                  height: "9px",
+                  bottom: "0",
+                  left: "0",
+                }}
+              ></div>
+            </div>
+            <motion.img
+              src="/aramco.png"
+              alt="Aramco Logo"
+              className="mx-auto"
+              style={{ width: "350px", height: "auto" }}
+              variants={scrollAnimation}
+            />
           </div>
-          <img
-            src="/aramco.png"
-            alt="Aramco Logo"
-            className="mx-auto"
-            style={{ width: '350px', height: 'auto' }}
-          />
-        </div>
 
-        {/* Organizer */}
-        <div className="text-right">
-          <div className="mb-4 relative inline-block">
-            <h3
-              className="text-2xl font-bold relative z-10"
-            >
-              الجهة المنظمة
-            </h3>
-            <div
-              className="absolute w-full bg-orange-500 z-0"
-              style={{
-                height: '9px',
-                bottom: '0',
-                left: '0',
-              }}
-            ></div>
+          {/* Organizer */}
+          <div className="text-right">
+            <div className="mb-4 relative inline-block">
+              <h3 className="text-2xl font-bold relative z-10">
+                الجهة المنظمة
+              </h3>
+              <div
+                className="absolute w-full bg-orange-500 z-0"
+                style={{
+                  height: "9px",
+                  bottom: "0",
+                  left: "0",
+                }}
+              ></div>
+            </div>
+            <motion.img
+              src="/juc.png"
+              alt="الهيئة الملكية للجبيل وينبع"
+              className="mx-auto"
+              style={{ width: "350px", height: "auto" }}
+              variants={scrollAnimation}
+            />
           </div>
-          <img
-            src="/juc.png"
-            alt="الهيئة الملكية للجبيل وينبع"
-            className="mx-auto"
-            style={{ width: '350px', height: 'auto' }}
-          />
-        </div>
-      </div>
+        </motion.div>
+      </ScrollAnimationWrapper>
 
       {/* Partners Section */}
-      <div className="flex flex-col w-full my-16">
-      <div className="mb-4 relative inline-block text-right" dir="rtl">
-  <h3 className="text-2xl font-bold relative z-10 inline-block">
-    شركـاء النجاح
-  </h3>
-  <div
-    className="absolute w-36 bg-orange-500 z-0" // Adjusted width to 'w-full' if full width is needed
-    style={{
-      height: '9px',
-      bottom: '0',
-      right: '0', // Changed from 'left' to 'right'
-    }}
-  ></div>
-</div>
-        
-        <ScrollAnimationWrapper>
-          <motion.div className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap" variants={scrollAnimation}>
+      <ScrollAnimationWrapper>
+        <motion.div className="flex flex-col w-full my-12" variants={scrollAnimation}>
+          <div className="mb-4 relative inline-block text-right" dir="rtl">
+            <h3 className="text-2xl font-bold relative z-10 inline-block">
+              شركـاء النجاح
+            </h3>
+            <div
+              className="absolute w-36 bg-orange-500 z-0"
+              style={{
+                height: "9px",
+                bottom: "0",
+                right: "0",
+              }}
+            ></div>
+          </div>
+
+          <motion.div
+            className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap"
+            variants={scrollAnimation}
+          >
             <img
               src="/assets/Icon/amazon.png"
               className="h-14 w-auto mt-4 lg:mt-2"
@@ -134,8 +152,8 @@ const Hero = () => {
               alt="Spotify Logo"
             />
           </motion.div>
-        </ScrollAnimationWrapper>
-      </div>
+        </motion.div>
+      </ScrollAnimationWrapper>
     </div>
   );
 };
