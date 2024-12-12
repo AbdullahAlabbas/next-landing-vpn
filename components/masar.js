ff9aa4bf5586e139d173987a2b60157be9d9d83c
Abdullah Alabbas <SEabdullahT@gmail.com>
2024-12-12 19:59:35 +0300

    .

--- components/masar.js
+++ components/masar.js
@@ -1,62 +1,54 @@
 import React, { useState } from "react";
-
-// import react slick
 import Slider from "react-slick";
 import Image from "next/image";
-import Stars from "../public/assets/Icon/stars.svg";
 import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
 import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
 
 const masar = ({
   listmasar = [
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "علم البيانات",
+      image: "/assets/icon-1.png",
       masar:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "تحليل البيانات الصناعية استخدام تقنيات علم البيانات لتحليل البيانات الضخمة من العمليات الصناعية بهدف تحسين الكفاءة وتوقع الأعطال",
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "الروبوتات",
+      image: "/assets/icon-2.png",
       masar:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        " تطوير واستخدام الروبوتات التي تعتمد على تقنيات متقدمة لتنفيذ المهام الصناعية، مما يساهم في تحسين كفاءة الإنتاج, تقليل التكاليف وزيادة الأمان في بيئات العمل",
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "سلاسل الإمداد",
+      image: "/assets/icon-3.png",
       masar:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        " سلاسل الإمداد الصناعية تشمل توريد المواد الخام، التصنيع، التخزين، والتوزيع، بهدف تحسين الكفاءة, تقليل التكاليف, تحسين التنبؤات وتبسيط العمليات اللوجستية ",
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "الذكاء الاصطناعي",
+      image: "/assets/icon-4.png",
       masar:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "تحسين العمليات الصناعية وتطوير تطبيقات تعتمد على تقنيات الذكاء الاصطناعي لزيادة كفاءة العمليات الصناعية الصناعية وتحسين الإنتاجية",
+    },
+    {
+      name: "الأمن السيبراني",
+      image: "/assets/icon-5.png",
+      masar:
+        "حماية البيانات والشبكات الصناعية تطوير حلول وبرامج تضمن أمان المعلومات والشبكات المستخدمة في العمليات الصناعية",
     },
   ],
+
 }) => {
   const settings = {
     dots: true,
     customPaging: function (i) {
       return (
-        <a className="">
-          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+        <a>
+          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all"></span>
         </a>
       );
     },
-    dotsClass: "slick-dots w-max absolute mt-20  ",
+    dotsClass: "slick-dots w-max absolute mt-20",
     infinite: true,
     speed: 500,
     slidesToShow: 3,
@@ -92,31 +84,19 @@ const masar = ({
         {listmasar.map((listmasars, index) => (
           <div className="px-3 flex items-stretch" key={index}>
             <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
-              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
-                <div className="flex order-2 xl:order-1">
-                  <Image
-                    src={listmasars.image}
-                    height={50}
-                    width={50}
-                    alt="Icon People"
-                  />
-                  <div className="flex flex-col ml-5 text-left">
-                    <p className="text-lg text-black-600 capitalize">
-                      {listmasars.name}
-                    </p>
-                    <p className="text-sm text-black-500 capitalize">
-                      {listmasars.city},{listmasars.country}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
-                  <p className="text-sm">{listmasars.rating}</p>
-                  <span className="flex ml-4">
-                    <Stars className="h-4 w-4" />
-                  </span>
+              <div className="flex flex-col w-full items-center">
+                <Image
+                  src={listmasars.image}
+                  height={55}
+                  width={55}
+                  alt="Icon"
+                  className="mb-3"
+                />
+                <div className="text-center">
+                  <p className="text-lg text-black-600 capitalize">{listmasars.name}</p>
                 </div>
               </div>
-              <p className="mt-5 text-left">“{listmasars.masar}”.</p>
+              <p className="mt-5 text-left">“{listmasars.masar}”</p>
             </div>
           </div>
         ))}
@@ -127,7 +107,7 @@ const masar = ({
             className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
             onClick={sliderRef?.slickPrev}
           >
-            <ArrowBack className="h-6 w-6 " />
+            <ArrowBack className="h-6 w-6" />
           </div>
           <div
             className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
